fb88f33de5547f7cfb9ac528063def9c569e4b51
Cesar Acual <agustoacual@gmail.com>
2022-05-24 11:55:25 -0600

    Reestructuracion del codigo y activacion de links

--- resources/views/admin/partials/nav.blade.php
+++ resources/views/admin/partials/nav.blade.php
@@ -2,24 +2,24 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-header">Navegación</li>
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="{{ route('dashboard') }}" class="{{ request()->is('admin') ? 'active' : '' }} nav-link">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
                     Inicio
                 </p>
             </a>
         </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
+        <li class="nav-item {{ request()->is('admin/posts') ? 'menu-open': '' }}">
+            <a href="#" class="nav-link {{ request()->is('admin/posts') ? 'active' : '' }}">
                 <i class="nav-icon fas fa-bars"></i>
                 <p>
                     Blog
                     <i class="fas fa-angle-left right"></i>
                 </p>
             </a>
-            <ul class="nav nav-treeview" style="display: none;">
+            <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="{{ route('admin.posts.index') }}" class="nav-link">
+                    <a href="{{ route('admin.posts.index') }}" class="nav-link {{ request()->is('admin/posts') ? 'active' : '' }}">
                         <i class="far fa-eye nav-icon"></i>
                         <p>Vet todos los posts</p>
                     </a>
