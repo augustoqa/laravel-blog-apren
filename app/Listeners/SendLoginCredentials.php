f062879af10294851e9e9d2bcbd83801bab036a8
Augusto Quisquinay <augustoacual@gmail.com>
2023-06-02 11:39:36 -0600

    Enviar las credenciales del usuario via email

--- app/Listeners/SendLoginCredentials.php
+++ app/Listeners/SendLoginCredentials.php
@@ -3,8 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\UserWasCreated;
+use App\Mail\LoginCredentials;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class SendLoginCredentials
 {
@@ -16,6 +18,8 @@ class SendLoginCredentials
      */
     public function handle(UserWasCreated $event)
     {
-        // Enviar el email con las credenciales del login
+        Mail::to($event->user)->queue(
+            new LoginCredentials($event->user, $event->password)
+        );
     }
 }
