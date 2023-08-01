28ff5b238da7d3acaa92531f2e83c48c7c376592
Augusto Quisquinay <augustoacual@gmail.com>
2023-08-01 15:56:55 -0600

    Mensajes de validación personalizados

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SaveRolesRequest;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -40,14 +41,9 @@ class RolesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(SaveRolesRequest $request)
     {
-        $data = $request->validate([
-            'name' => 'required|unique:roles',
-            'display_name' => 'required',
-        ]);
-
-        $role = Role::create($data);
+        $role = Role::create($request->validated());
 
         $role->givePermissionTo($request->permissions);
 
@@ -88,13 +84,9 @@ class RolesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Role $role)
+    public function update(SaveRolesRequest $request, Role $role)
     {
-        $data = $request->validate([
-            'display_name' => 'required',
-        ]);
-
-        $role->update($data);
+        $role->update($request->validated());
 
         $role->givePermissionTo($request->permissions);
 
