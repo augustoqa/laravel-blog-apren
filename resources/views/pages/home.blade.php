f4d4cc5f9236892b49403d33620c910ea5f91ad7
Augusto Quisquinay <augustoacual@gmail.com>
2023-08-17 13:25:52 -0600

    Mostrar autores, categorias y posts en la pagina archivo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
         @if (isset($title))
         <h3>{{ $title }}</h3>
         @endif
-        @foreach($posts as $post)
+        @forelse($posts as $post)
         <article class="post">
 
             @include($post->viewType('home'))
@@ -13,7 +13,7 @@
             <div class="content-post">
 
                 @include('posts.header')
-                
+
                 <h1>{{ $post->title }}</h1>
                 <div class="divider"></div>
                 <p>{{ $post->excerpt }}</p>
@@ -27,11 +27,16 @@
                 </footer>
             </div>
         </article>
-        @endforeach
+        @empty
+        <article class="post">
+            <div class="content-post">
+                <h1>No hay publicaciones todavía.</h1>
+            </div>
+        </article>
+        @endforelse
     </section><!-- fin del div.posts.container -->
 
     {{ $posts->links() }}
 
 @stop
 
-    
